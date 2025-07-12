faf568202091d7c6d91719e56b12260bbc7a93a9
KorobovS <korobovs33@yandex.ru>
2025-07-12 23:09:49 +0300

    check menLinkClick

--- src/test/java/ui/pages/MenPage.java
+++ src/test/java/ui/pages/MenPage.java
@@ -15,7 +15,8 @@ public class MenPage extends BasePage {
         super(driver);
     }
 
-    @FindAll(@FindBy(xpath = "//div[@data-testid='product-content']/div/div/div/a"))
+    @FindAll(@FindBy(xpath = "//div[@data-testid='product-content']/div/div/div/a[@data-testid='xm-link']/div[@class='product-tile-image-container']/.."))
+//    @FindAll(@FindBy(xpath = "//div[@data-testid='product-content']/div/div/div/a"))
     private List<WebElement> listProduct;
 
     @Step("Кликаю по первому товару")
@@ -40,22 +41,22 @@ public class MenPage extends BasePage {
     @Step("Кликаю по товару под номером {numberInProductArray}")
     public ProductPage productClick(int numberInProductArray) {
 
-        Allure.step("1");
+        Allure.step("4");
         menLinkClick();
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        Allure.step("2");
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        Allure.step("4");
-        js.executeScript("arguments[0].scrollIntoView(true);", listProduct);
         Allure.step("5");
-        List<WebElement> products = driver.findElements(By.xpath("//div[@data-testid='product-content']/div/div/div/a[@data-testid='xm-link']/div[@class='product-tile-image-container']/.."));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         Allure.step("6");
-        WebElement product = products.get(numberInProductArray);
+        js.executeScript("arguments[0].scrollIntoView(true);", listProduct);
         Allure.step("7");
+        List<WebElement> products = driver.findElements(By.xpath("//div[@data-testid='product-content']/div/div/div/a[@data-testid='xm-link']/div[@class='product-tile-image-container']/.."));
+        Allure.step("8");
+        WebElement product = products.get(numberInProductArray);
+        Allure.step("9");
         product.click();
 
         return new ProductPage(driver);
