05a65d4ef6688c13809519738917f5a9fad0ec90
KorobovS <korobovs33@yandex.ru>
2025-07-12 16:31:49 +0300

    update screenshots

--- src/test/java/ui/utils/ProjectUtils.java
+++ src/test/java/ui/utils/ProjectUtils.java
@@ -14,38 +14,41 @@ import java.util.Map;
 
 public class ProjectUtils {
 
-    private static final ChromeOptions CHROME_OPTIONS;
 
     public static WebDriver createDriver() {
 
+        ChromeOptions chromeOptions = new ChromeOptions();
         WebDriver driver;
         String remoteUrl = System.getenv("SELENIUM_REMOTE_URL");
 
         LoggerUtil.info(String.format("SELENIUM_REMOTE_URL = %s", remoteUrl));
-        if ((remoteUrl != null) || !remoteUrl.isEmpty()) {
+        if (remoteUrl != null) {
             Allure.addAttachment("RemoteUrl", remoteUrl);
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless");
-            options.addArguments("--disable-gpu");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
-            options.setCapability("goog:loggingPrefs", Map.of("browser", "ALL"));
+//            chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--headless");
+            chromeOptions.addArguments("--disable-gpu");
+            chromeOptions.addArguments("--no-sandbox");
+            chromeOptions.addArguments("--disable-dev-shm-usage");
+            chromeOptions.setCapability("goog:loggingPrefs", Map.of("browser", "ALL"));
             try {
-                driver = new RemoteWebDriver(new URL(remoteUrl), options);
+                driver = new RemoteWebDriver(new URL(remoteUrl), chromeOptions);
             } catch (MalformedURLException e) {
                 throw new RuntimeException("Malformed URL for Selenium Remote WebDriver", e);
             }
         } else {
             Allure.addAttachment("Local run", "No remote driver");
-            driver = new ChromeDriver(CHROME_OPTIONS);
+//            chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--window-size=1920,1080");
+            chromeOptions.setPageLoadStrategy(PageLoadStrategy.EAGER);
+            driver = new ChromeDriver(chromeOptions);
         }
 
         return driver;
     }
-
-    static {
-        CHROME_OPTIONS = new ChromeOptions();
-        CHROME_OPTIONS.addArguments("--window-size=1920,1080");
-        CHROME_OPTIONS.setPageLoadStrategy(PageLoadStrategy.EAGER);
-    }
+//
+//    static {
+//        CHROME_OPTIONS = new ChromeOptions();
+//        CHROME_OPTIONS.addArguments("--window-size=1920,1080");
+//        CHROME_OPTIONS.setPageLoadStrategy(PageLoadStrategy.EAGER);
+//    }
 }
