ecf446d76334a6b02ba190bf0aba5ec93637ddf4
KorobovS <korobovs33@yandex.ru>
2025-07-12 23:21:39 +0300

    check menLinkClick

--- src/test/java/ui/pages/MenPage.java
+++ src/test/java/ui/pages/MenPage.java
@@ -19,27 +19,27 @@ public class MenPage extends BasePage {
 //    @FindAll(@FindBy(xpath = "//div[@data-testid='product-content']/div/div/div/a"))
     private List<WebElement> listProduct;
 
-    @Step("Кликаю по первому товару")
-    public ProductPage productFirstClick() {
-
-        menLinkClick();
-        Allure.step("0");
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        Allure.step("1");
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-                Allure.step("1.1");
-                WebElement element = listProduct.get(0);
-        Allure.step("2");
-        js.executeScript("arguments[0].scrollIntoView(true);", element);
-        Allure.step("3");
-        js.executeScript("arguments[0].click();", listProduct.get(0));
-
-        return new ProductPage(driver);
-    }
+//    @Step("Кликаю по первому товару")
+//    public ProductPage productFirstClick() {
+//
+//        menLinkClick();
+//        Allure.step("0");
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//        Allure.step("1");
+//        JavascriptExecutor js = (JavascriptExecutor) driver;
+//                Allure.step("1.1");
+//                WebElement element = listProduct.get(0);
+//        Allure.step("2");
+//        js.executeScript("arguments[0].scrollIntoView(true);", element);
+//        Allure.step("3");
+//        js.executeScript("arguments[0].click();", listProduct.get(0));
+//
+//        return new ProductPage(driver);
+//    }
 
     @Step("Кликаю по товару под номером {numberInProductArray}")
     public ProductPage productClick(int numberInProductArray) {
@@ -52,14 +52,12 @@ public class MenPage extends BasePage {
             throw new RuntimeException(e);
         }
         Allure.step("5");
-        JavascriptExecutor js = (JavascriptExecutor) driver;
+        WebElement product = listProduct.get(numberInProductArray);
         Allure.step("6");
-        js.executeScript("arguments[0].scrollIntoView(true);", listProduct);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         Allure.step("7");
-        List<WebElement> products = driver.findElements(By.xpath("//div[@data-testid='product-content']/div/div/div/a[@data-testid='xm-link']/div[@class='product-tile-image-container']/.."));
+        js.executeScript("arguments[0].scrollIntoView(true);", listProduct);
         Allure.step("8");
-        WebElement product = products.get(numberInProductArray);
-        Allure.step("9");
         product.click();
 
         return new ProductPage(driver);
