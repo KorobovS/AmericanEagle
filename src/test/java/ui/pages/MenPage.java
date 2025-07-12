d50a72196b49bc10a27f1dbf6ade3d35ffc82d74
KorobovS <korobovs33@yandex.ru>
2025-07-12 20:10:39 +0300

    update productFirstClick

--- src/test/java/ui/pages/MenPage.java
+++ src/test/java/ui/pages/MenPage.java
@@ -20,11 +20,11 @@ public class MenPage extends BasePage {
     public ProductPage productFirstClick() {
 
         menLinkClick();
-//        try {
-//            Thread.sleep(5000);
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].scrollIntoView(true);", productFirst);
         wait.until(ExpectedConditions.elementToBeClickable(productFirst));
