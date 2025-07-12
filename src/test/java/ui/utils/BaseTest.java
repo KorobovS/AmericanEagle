8f32674ebdef7afb55794c1ec6ec49c506337754
KorobovS <korobovs33@yandex.ru>
2025-07-12 16:51:41 +0300

    update screenshots

--- src/test/java/ui/utils/BaseTest.java
+++ src/test/java/ui/utils/BaseTest.java
@@ -1,6 +1,5 @@
 package ui.utils;
 
-import io.qameta.allure.Allure;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -10,7 +9,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import utils.LoggerUtil;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Method;
@@ -48,23 +46,16 @@ public abstract class BaseTest {
     @AfterMethod
     protected void afterMethod(Method method, ITestResult testResult) {
 
-//        if (testResult.getStatus() == ITestResult.FAILURE) {
-//            TakesScreenshot ts = (TakesScreenshot) driver;
-//            File source = ts.getScreenshotAs(OutputType.FILE);
-//            String destination = "screenshots/" + testResult.getName() + "_" + System.currentTimeMillis() + ".png";
-//            try {
-//                FileUtils.copyFile(source, new File(destination));
-//                System.out.println("Скриншот сохранен в: " + destination);
-//            } catch (IOException e) {
-//                System.err.println("Ошибка при сохранении скриншота: " + e.getMessage());
-//            }
-//        }
-        if (!testResult.isSuccess()) {
-            Allure.addAttachment(
-                    "screenshot.png",
-                    "image/png",
-                    new ByteArrayInputStream(((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.BYTES)),
-                    "png");
+        if (testResult.getStatus() == ITestResult.FAILURE) {
+            TakesScreenshot ts = (TakesScreenshot) driver;
+            File source = ts.getScreenshotAs(OutputType.FILE);
+            String destination = "screenshots/" + testResult.getName() + "_" + System.currentTimeMillis() + ".png";
+            try {
+                FileUtils.copyFile(source, new File(destination));
+                System.out.println("Скриншот сохранен в: " + destination);
+            } catch (IOException e) {
+                System.err.println("Ошибка при сохранении скриншота: " + e.getMessage());
+            }
         }
 
         closeDriver();
